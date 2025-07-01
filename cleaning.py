7b25d90a0bd1afe40166f9e9ada8da55c4e25429
alizacotton <159484305+alizacotton@users.noreply.github.com>
2025-07-01 11:22:34 -0400

    Add files via upload

---
+++ cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_excel('C:/Users/aliza/Downloads/Grant+List+2024.xlsx', usecols="I,J,K,L")
+
+df = df[df['Propert Description'].str.contains('DWL')]
+
+#df["Street Address"] = df["Road #"] + " " + df["Road Name"] + "Charlotte, VT 05445"
+df["Road #"] = df["Road #"].astype(str)
+
+df["Street Address"] = df[["Road #","Road Name"]].agg(" ".join, axis=1)
+
+df["Street Address"] = df["Street Address"] + ", Charlotte VT, 05445"
+
+df = df.drop("Road #", axis=1)
+df = df.drop("Road Name", axis=1)
+df = df.drop("Tax Map Reference", axis=1)
+df = df.drop("Propert Description", axis=1)
+
+filepath= 'C:/Users/aliza/OneDrive/charlotte-project/Better-addresses.csv'
+df.to_csv(filepath, index=False)
+
+a = df.head()
+print (a)
